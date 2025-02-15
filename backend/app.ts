3823dc2db02bce36bcd0c7a08b53ef8d24dd9d34
shawnjiang1019 <shawnj1019@gmail.com>
2025-02-14 22:03:29 -0500

    initialize server

---
+++ backend/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
